0781d3b429853d9aff3f6ad9fb7c4dfbe54c17d3
Trương Nho Thông <thongmo2602@hotmail.com>
2018-09-04 09:24:57 +0700

    'update'

--- app/src/main/java/com/example/thong/fragment/fragment_giohang.java
+++ app/src/main/java/com/example/thong/fragment/fragment_giohang.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v7.widget.GridLayoutManager;
@@ -14,7 +15,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import com.example.thong.APIs;
 import com.example.thong.adapter.Adapter_GioHang;
@@ -26,33 +29,34 @@ import java.util.ArrayList;
 
 public class fragment_giohang extends Fragment {
     SQLiteDatabase database;
-    ArrayList<GioHang>dsgh =new ArrayList<>();
+    ArrayList<GioHang> dsgh = new ArrayList<>();
     RecyclerView recyclerView;
     Adapter_GioHang adapter;
     View view;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        view =inflater.inflate(R.layout.fragment_giohang,container,false);
+        view = inflater.inflate(R.layout.fragment_giohang, container, false);
         getData();
-        recyclerView=view.findViewById(R.id.list_giohang);
-        GridLayoutManager gridLayoutManager =new GridLayoutManager(getActivity(),1);
+        recyclerView = view.findViewById(R.id.list_giohang);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 1);
         recyclerView.setLayoutManager(gridLayoutManager);
-        adapter=new Adapter_GioHang(dsgh);
+        adapter = new Adapter_GioHang(dsgh);
         recyclerView.setAdapter(adapter);
-        if(dsgh.size()<=0){
-            Toast.makeText(getActivity(),"Giỏ hàng của bạn rỗng !",Toast.LENGTH_LONG).show();
+
+        if (dsgh.size() <= 0) {
+            Toast.makeText(getActivity(), "Giỏ hàng của bạn rỗng !", Toast.LENGTH_LONG).show();
         }
-        return  view;
+        return view;
     }
 
-
-    private void getData(){
+    private void getData() {
         dsgh.clear();
-        database=getActivity().openOrCreateDatabase(APIs.database_name,Context.MODE_PRIVATE,null);
-        Cursor cursor =database.rawQuery("select * from GioHang",null);
-        while (cursor.moveToNext()){
-            GioHang gh =new GioHang();
+        database = getActivity().openOrCreateDatabase(APIs.database_name, Context.MODE_PRIVATE, null);
+        Cursor cursor = database.rawQuery("select * from GioHang", null);
+        while (cursor.moveToNext()) {
+            GioHang gh = new GioHang();
             gh.setMasp(cursor.getString(1));
             gh.setTensp(cursor.getString(2));
             gh.setAnh(cursor.getString(3));
@@ -70,10 +74,9 @@ public class fragment_giohang extends Fragment {
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
-        if(isVisibleToUser){
+        if (isVisibleToUser) {
 
-        }
-        else {
+        } else {
             view.clearFocus();
         }
     }
